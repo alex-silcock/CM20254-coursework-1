41dafd3e2f362257b3e26518b578dd0d0f2ca6d6
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2022-11-20 18:56:10 +0000

    Added files

---
+++ src/Hashtable.java
@@ -0,0 +1,23 @@
+public class Hashtable {
+
+    public Hashtable(int size) {
+	// Add code here for your constructor
+    }
+
+    public void add(String key) {
+	// Add code here for your add method, using linear probing to resolve collisions
+    }
+
+    public boolean search(String key) {
+	// Add code here for your search method
+    }
+
+    public void delete(String key) {
+	// Add code here for your delete method
+    }
+
+    public String get(int i) {
+	// Add code here for your get method
+    }
+
+}
