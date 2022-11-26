1cc34efee0f82622cec50194916753f726b877fb
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2022-11-26 18:33:39 +0000

    New tests: deleteFromFullTableOfSameValues, addSameString, caseSensitiveAdd, checkInitialValuesAreNull, deleteEmptyTable

--- test/HashtableTests.java
+++ test/HashtableTests.java
@@ -30,6 +30,7 @@ public class HashtableTests {
         table.add("apple4");
         assertTrue(table.search("apple4"));
     }
+
     @Test
     public void precheck3() {
         Hashtable table = new Hashtable(8);
@@ -69,7 +70,6 @@ public class HashtableTests {
         assertEquals("abcdefg", table.get(2));
         assertEquals("abcdegH", table.get(3));
         assertEquals("abcdef", table.get(4));
-
     }
 
     @Test
@@ -83,7 +83,6 @@ public class HashtableTests {
         }
         assertEquals("Apple2", table.get(2));
         table.delete("Apple1");
-        table.printTable();
         assertTrue(table.search("Apple2"));
     }
 
@@ -103,6 +102,45 @@ public class HashtableTests {
     }
 
     @Test
+    public void deleteFromFullTableOfSameValues() {
+        Hashtable table = new Hashtable(8);
+        String[] words = {"AaAaAa" , "AaAaBB",  "AaBBAa",  "AaBBBB",  "BBAaAa",  "BBAaBB",  "BBBBAa", "BBBBBB"};
+
+        for (String s : words) {
+            table.add(s);
+        }
+
+        table.delete("AaAaAa");
+        assertEquals("AaAaBB", table.get(0));
+        assertEquals("AaBBAa", table.get(1));
+        assertEquals("AaBBBB", table.get(2));
+        assertEquals("BBAaAa", table.get(3));
+        assertEquals("BBAaBB", table.get(4));
+        assertEquals("BBBBAa", table.get(5));
+        assertEquals("BBBBBB", table.get(6));
+        assertNull(table.get(7));
+    }
+
+    @Test
+    public void deleteFromEndAndMoveStartBack() {
+        Hashtable table = new Hashtable(8);
+        String[] words = {"AaAaAaa" , "AaAaBBa",  "AaBBAaa",  "AaBBBBa",  "BBAaAaa",  "BBAaBBa",  "BBBBAaa", "apple0"};
+
+        for (String s : words) {
+            table.add(s);
+        }
+        table.delete("AaAaAaa");
+        assertNull(table.get(0));
+        assertEquals("AaAaBBa", table.get(1));
+        assertEquals("AaBBAaa", table.get(2));
+        assertEquals("AaBBBBa", table.get(3));
+        assertEquals("BBAaAaa", table.get(4));
+        assertEquals("BBAaBBa", table.get(5));
+        assertEquals("BBBBAaa", table.get(6));
+        assertEquals("apple0", table.get(7));
+    }
+
+    @Test
     public void addToFullTable() {
         Hashtable table = new Hashtable(4);
         String[] words = {"apple1", "apple2", "apple3", "apple4"};
@@ -116,7 +154,38 @@ public class HashtableTests {
         assertEquals(words[3], table.get(2));
         assertEquals(words[0], table.get(3));
         assertFalse(table.search("apple5"));
+    }
 
+    @Test
+    public void addSameString() {
+        Hashtable table = new Hashtable(6);
+        String[] words = {"Hello", "Hello", "Hello", "Hello", "Hello", "Hello"};
+
+        for (String s : words) {
+            table.add(s);
+        }
+        assertNull(table.get(0));
+        assertNull(table.get(1));
+        assertEquals("Hello", table.get(2));
+        assertNull(table.get(3));
+        assertNull(table.get(4));
+        assertNull(table.get(5));
+    }
+
+    @Test
+    public void caseSensitiveAdd() {
+        Hashtable table = new Hashtable(6);
+        String[] words = {"Hello", "hello", "HELLO", "HeLlO", "hElLo", "HEllO"};
+
+        for (String s : words) {
+            table.add(s);
+        }
+        assertEquals("HELLO", table.get(0));
+        assertEquals("hElLo", table.get(1));
+        assertEquals("Hello", table.get(2));
+        assertEquals("HEllO", table.get(3));
+        assertEquals("hello", table.get(4));
+        assertEquals("HeLlO", table.get(5));
     }
 
     @Test
@@ -131,4 +200,22 @@ public class HashtableTests {
         assertFalse(table.search("apple5"));
     }
 
+    @Test
+    public void checkInitialValuesAreNull() {
+        Hashtable table = new Hashtable(10);
+
+        for (int i = 0; i < 10; i++) {
+            assertNull(table.get(i));
+        }
+    }
+
+    @Test
+    public void deleteEmptyTable() {
+        Hashtable table = new Hashtable(5);
+        table.delete("");
+
+        for (int i = 0; i < 5; i++) {
+            assertNull(table.get(i));
+        }
+    }
 }
